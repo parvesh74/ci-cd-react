9f9d973baac5331db8f920af4491c2639fe6025c
parvesh74 <131499170+parvesh74@users.noreply.github.com>
2025-04-07 14:26:51 -0700

    Add course name to the page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Parveshdeep</h1>
+        <h1>Parvesh Mann</h1>
         <p>Software Development</p>
       </header>
     </div>
