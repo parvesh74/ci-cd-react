68b405c3db14eee00caddb44bc86abc33693a685
parvesh74 <131499170+parvesh74@users.noreply.github.com>
2025-04-07 14:56:16 -0700

    fix 2

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders Parveshdeep on the screen', () => {
+test('renders Parvesh Mann on the screen', () => {
   render(<App />);
-  const nameElement = screen.getByText(/Parveshdeep/i);
+  const nameElement = screen.getByText(/Parvesh Mann/i);
   expect(nameElement).toBeInTheDocument();
+});
+
+test('renders Software Development on the screen', () => {
+  render(<App />);
+  const courseElement = screen.getByText(/Software Development/i);
+  expect(courseElement).toBeInTheDocument();
 });
