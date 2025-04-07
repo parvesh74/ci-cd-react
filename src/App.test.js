55ad7ec7b20455d240ed2ac6c069670f7b46bd23
parvesh74 <131499170+parvesh74@users.noreply.github.com>
2025-04-07 13:25:09 -0700

    Added unit test to check for name on screen

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Parveshdeep on the screen', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameElement = screen.getByText(/Parveshdeep/i);
+  expect(nameElement).toBeInTheDocument();
 });
